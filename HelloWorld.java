af94f651536b227cfe90f225a4ce192d4c2286c5
rajeev-asthana <68969240+rajeev-asthana@users.noreply.github.com>
2021-05-08 03:57:18 +0530

    Update HelloWorld.java
    updated text to trigger webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 
 for( int i=1;i<=10;i++ )
   {
-   System.out.println("Hello World from Webhook ...."+i);
+   System.out.println("Hello World from awesome Webhook ...."+i);
   }
 
  }
